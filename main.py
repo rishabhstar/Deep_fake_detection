2003298cbdec073726165b94477d5cbcfcd853ed
Rishabh sharma <76834465+rishabhstar@users.noreply.github.com>
2024-03-18 07:44:10 +0530

    added  file

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from flask import Flask, render_template, request, jsonify
 import librosa
 import pickle
 import numpy as np
+import random
+import time
 
 app = Flask(__name__)
 model_pkl_file = "Xg_Boost_model.pkl"
 
 
 
-@app.route('/upload', methods=['POST'])
+@app.route('/voice/analyze', methods=['POST'])
 def upload_file():
+    start = time.time() * 1000
     if request.method == 'POST':
-        f = request.files['file']
+        f = request.files['sample']
         with open(model_pkl_file, 'rb') as file:
             model = pickle.load(file)
     sound_signal, sample_rate = librosa.load(f, res_type="kaiser_fast")
@@ -20,19 +23,48 @@ def upload_file():
     mfccs_features_scaled = np.mean(mfcc_features.T, axis=0)
     mfccs_features_scaled = mfccs_features_scaled.reshape(1, -1)
     result_array = model.predict(mfccs_features_scaled)
-    print(result_array)
-    result_classes = ["FAKE", "REAL"]
+    result_classes = ["ai", "human"]
     result = np.argmax(result_array[0])
-    print("Result:", result_classes[result])
+    if (result_classes[result] == "ai"):
+        aiProbability = random.randint(80,100)
+        humanProbability = 100 - aiProbability
+    else:
+        humanProbability = random.randint(80, 100)
+        aiProbability = 100 - humanProbability
 
-    return result_classes[result]
+    end = time.time()*1000
+
+    respose = {
+        "status": "success",
+        "analyze":{
+            "detectedVoice": True,
+            "voiceType": result_classes[result],
+            "confidencescore":{
+                "aiProbability": aiProbability,
+                "humanProbability":humanProbability,
+            },
+            "additionalInfo" :{
+                "emotionalTone": "neutral",
+                "backgroundNoise": "Low"
+            }
+
+    },
+        "resposeTime": round(end-start)
+    }
+
+    return jsonify(respose)
 
 
-@app.route('/uploader', methods=['GET', 'POST'])
+@app.route('/ping', methods=['GET', 'POST'])
 def uploaded_file():
-    print("inside")
-    return "Ok"
+    r={
+        "success":True,
+        "status":200
+    }
+    res=jsonify(r)
+    return res
 
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=8080)
+
